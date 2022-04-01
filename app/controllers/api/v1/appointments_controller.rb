aa7ea8881a2436470f8eda114dc51a504481f9fa
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-01 20:54:46 +0100

    Add create appointment method

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -6,6 +6,17 @@ class Api::V1::AppointmentsController < ApplicationController
     render json: @appointment
   end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      DoctorAppointment.create(doctor_id: params[:doctor_id], appointment_id: params[:@appointment.id])
+      render json: @appointment, status: :created
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
+  
+
   private
   def appointment_params
     params.require(:appointment).permit(:location, :dateOfAppointment, :user_id, :doctor_id)
