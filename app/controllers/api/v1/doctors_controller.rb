eb541d0cbfc589224b0a1878b0b37940874a4940
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-01 18:23:58 +0200

    Add render doctror save

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -8,4 +8,12 @@ class Api::V1::DoctorsController < ApplicationController
     @doctors = Doctor.find_by(id: params[:id])
     render json: @doctors
   end
+
+  def create
+    doctor = Doctor.new(params.permit(:name, :dscription, :price, :image))
+    if doctor.save
+      render json: doctor, status: :created
+    else 
+      render json: {error: }
+
 end
