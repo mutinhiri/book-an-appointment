0eedcbe52e77e0d3bdbb5ffe031f20d8e9a0d782
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-01 11:03:24 +0100

    Fix linter error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
-  def new
-    
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = 'You have successfully logged in'
     else
-      flash.now[:danger] = "Username / or password incorrect!"
+      flash.now[:danger] = 'Username / or password incorrect!'
     end
-    
   end
-  
+
   def destroy
     session[:user_id] = nill
     flash[:success] = 'You have logged out'
   end
-  
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,19 +8,15 @@ class Api::V1::UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'Signup was successful'
-      #redirect_to Something_path
+      # redirect_to Something_path
     else
       flash[:danger] = 'Cannot signup. Try again!'
     end
-    
   end
-  
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
-  
-  
 end
