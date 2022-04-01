fd740c4357e7edfa96b90acd3a8953f9ac53472f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-04-01 09:51:19 +0100

    Add secure password to user

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
+gem 'bcrypt', '~> 3.1', '>= 3.1.17'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+
+  before_save {self.email = email.downcase}
   validates :name, presence: true, length: {minimum: 3, maximum: 100}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false},
              format: {with: VALID_EMAIL_REGEX}
-  
+  has_secure_password
   
 end
