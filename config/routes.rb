3184baa2a2100b9a0d69902c89ccacc664a75886
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-04-04 13:12:13 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get 'appointments' to: 'appointments#index'
-  post 'appointments' to: 'appointments#create'
-  delete 'appointments/:id' to: 'appointments#destroy'
-
+  get 'appointments', to: 'appointments#index'
+  post 'appointments', to: 'appointments#create'
+  delete 'appointments/:id', to: 'appointments#destroy'
 end
